02866818a979033b800b8fc71513cfafa2b10cc9
Beviryon ISSANGA <viryon.ngoulou@gmail.com>
2026-05-31 01:18:09 +0200

    implementation avec firebase

--- config.example.js
+++ config.example.js
@@ -6,15 +6,15 @@
  */
 window.PFF_CONFIG = {
   firebase: {
-    apiKey: 'VOTRE_API_KEY',
-    authDomain: 'votre-projet.firebaseapp.com',
-    projectId: 'votre-projet',
-    storageBucket: 'votre-projet.appspot.com',
-    messagingSenderId: '123456789',
-    appId: '1:123456789:web:abcdef',
-    measurementId: 'G-XXXXXXXXXX',
+    apiKey: "AIzaSyC3hoiIXh67dt6LgJwNxEmFLm4uWZUBi2o",
+    authDomain: "creneau-trevixia.firebaseapp.com",
+    projectId: "creneau-trevixia",
+    storageBucket: "creneau-trevixia.firebasestorage.app",
+    messagingSenderId: "99772802044",
+    appId: "1:99772802044:web:c53a5174056d25ef148879",
+    measurementId: "G-WGZWMJXGTF"
   },
   adminEmails: [
-    'admin@trevixia.com',
+    'trevixiacontact@gmail.com',
   ],
 };
